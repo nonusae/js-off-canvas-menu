85e525f0316a10aaa8290f409b5141b8d00075d9
Natthakit <nonusae@gmail.com>
2018-02-05 17:47:40 +0700

    First implementation

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// START EDITING YOUR JAVASCRIPT HERE
+const offsiteToggle = document.querySelector('.jsOffsiteToggle')
+offsiteToggle.addEventListener('click', e => document.body.classList.toggle('offsite-is-open'));
+
+// ===============
